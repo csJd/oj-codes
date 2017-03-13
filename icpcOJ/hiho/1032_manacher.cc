cb39b0ad4bbccb4ca3f6812bf9f0854dda40a95e
Deng <d.vay@qq.com>
2017-03-13 10:05:39 +0800

    refacted manacher

--- icpcOJ/hiho/1032_manacher.cc
+++ icpcOJ/hiho/1032_manacher.cc
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
 using namespace std;
 const int L = 1e6 + 10;
@@ -8,20 +7,12 @@ int d[L << 1]; //d[i]记录以i为中心的回文子串在i右边的长度
 
 int manacher()
 {
-    int ret = 0;
+    int ret = 0; //最长回文字串的长度
     int j = 0; //记录j + d[j]的最远位置
-    d[0] = 0;
     for(int i = 1; s[i]; ++i)
     {
         d[i] = min(d[j * 2 - i], d[j] - (i - j));
-        int l = i - d[i];
-        int r = i + d[i];
-        while(l >= 0 && s[r] && s[l] == s[r])
-        {
-            --l;
-            ++r;
-        }
-        d[i] = r - 1 - i;
+        while(i - d[i] && s[i + d[i] + 1] == s[i - d[i] - 1]) ++d[i];
         if(i + d[i] > j + d[j]) j = i;
         if(d[i] > ret) ret = d[i];
     }
@@ -35,18 +26,16 @@ int main()
     while(T--)
     {
         scanf("%s", t);
-        s[0] = '@';
+        s[0] = 'y';
         int l = 0;
         for(int i = 0; t[i]; ++i)
         {
             s[++l] = t[i];
-            s[++l] = '@';
+            s[++l] = 'y';
         }
         s[++l] = 0;
-
         printf("%d\n", manacher());
-
     }
     return 0;
 }
-//Last modified :  2017-03-12 14:57 CST
+//Last modified :  2017-03-13 10:02 CST
