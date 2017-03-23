a1831c703daa4d13906c9dfc7bade99d1203e745
Deng <d.vay@qq.com>
2017-03-23 14:51:28 +0800

    modified sql

--- sql/scut_reexam/2016/create.sql
+++ sql/scut_reexam/2016/create.sql
@@ -1,11 +1,8 @@
-CREATE TABLE students
+CREATE TABLE departments
 (
-    sno VARCHAR(5),
-    sname VARCHAR(20),
-    sdate DATE,
-    cno VARCHAR(5),
-    PRIMARY KEY (sno),
-    FOREIGN KEY (cno) REFERENCES classes(cno)
+    dno VARCHAR(5),
+    dname VARCHAR(20),
+    PRIMARY KEY (dno)
 );
 
 CREATE TABLE classes
@@ -16,12 +13,23 @@ CREATE TABLE classes
     cyear INT,
     cnum INT CHECK (cnum < 31),
     PRIMARY KEY (cno),
-    FOREIGN KEY (don) REFERENCES departments(cno)
+    FOREIGN KEY (dno) REFERENCES departments(dno)
 );
 
-CREATE TABLE departments
+CREATE TABLE students
 (
-    dno VARCHAR(5),
-    dname VARCHAR(20),
-    PRIMARY KEY (dno)
+    sno VARCHAR(5),
+    sname VARCHAR(20),
+    -- sname VARCHAR(20) NOT NULL,
+    sdate DATE,
+    cno VARCHAR(5),
+    PRIMARY KEY (sno),
+    FOREIGN KEY (cno) REFERENCES classes(cno)
+    -- FOREIGN KEY (cno) REFERENCES classes(con) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+ALTER TABLE students ALTER COLUMN sname VARCHAR(20) NOT NULL;
+ALTER TABLE students ADD CONSTRAINT m_cascade
+  FOREIGN KEY (cno) REFERENCES classes(cno)
+  ON DELETE CASCADE ON UPDATE CASCADE;
+-- 删除约束就是 ALTER TABLE students DROP CONSTRAINT m_cascade
