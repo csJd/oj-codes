825698ad4cbcd03ec4475029ef7f73fd9a8c4285
Deng <scutjd@gmail.com>
2017-07-12 21:26:45 +0800

    Add leetcode problem 1 better solution

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -25,3 +25,13 @@ class Solution:
             j = binSearch(iNums[i + 1:], target - iNums[i][1])
             if j >= 0:
                 return sorted([iNums[i][0], j])
+
+class BetterSolution:
+     def twoSum(self, nums, target):
+        dic = {}
+        l = len(nums)
+        for i in range(l):
+            if nums[i] in dic:
+                return [dic[nums[i]], i]
+            else:
+                dic[target - nums[i]] = i
