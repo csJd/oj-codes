e4207ed44f0f7cb927310a5a2aeff603892538dc
Deng <d.vay@qq.com>
2019-06-03 20:34:10 +0800

    Add leetcode 61-63

---
+++ leetcode/62.unique-paths.py
@@ -0,0 +1,60 @@
+#
+# @lc app=leetcode id=62 lang=python3
+#
+# [62] Unique Paths
+#
+# https://leetcode.com/problems/unique-paths/description/
+#
+# algorithms
+# Medium (47.18%)
+# Likes:    1544
+# Dislikes: 109
+# Total Accepted:    288.2K
+# Total Submissions: 605.5K
+# Testcase Example:  '3\n2'
+#
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+# the diagram below).
+#
+# The robot can only move either down or right at any point in time. The robot
+# is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+# the diagram below).
+#
+# How many possible unique paths are there?
+#
+#
+# Above is a 7 x 3 grid. How many possible unique paths are there?
+#
+# Note: m and n will be at most 100.
+#
+# Example 1:
+#
+#
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the
+# bottom-right corner:
+# 1. Right -> Right -> Down
+# 2. Right -> Down -> Right
+# 3. Down -> Right -> Right
+#
+#
+# Example 2:
+#
+#
+# Input: m = 7, n = 3
+# Output: 28
+#
+#
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n+1) for i in range(m+1)]
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                dp[i+1][j] += dp[i][j]
+                dp[i][j+1] += dp[i][j]
+        return dp[m-1][n-1]
---
+++ leetcode/63.unique-paths-ii.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=63 lang=python3
+#
+# [63] Unique Paths II
+#
+# https://leetcode.com/problems/unique-paths-ii/description/
+#
+# algorithms
+# Medium (33.37%)
+# Likes:    855
+# Dislikes: 119
+# Total Accepted:    201.3K
+# Total Submissions: 601.6K
+# Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+#
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+# the diagram below).
+#
+# The robot can only move either down or right at any point in time. The robot
+# is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+# the diagram below).
+#
+# Now consider if some obstacles are added to the grids. How many unique paths
+# would there be?
+#
+#
+#
+# An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+#
+# Note: m and n will be at most 100.
+#
+# Example 1:
+#
+#
+# Input:
+# [
+# [0,0,0],
+# [0,1,0],
+# [0,0,0]
+# ]
+# Output: 2
+# Explanation:
+# There is one obstacle in the middle of the 3x3 grid above.
+# There are two ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down -> Down
+# 2. Down -> Down -> Right -> Right
+#
+#
+#
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0] * (n+1) for i in range(m+1)]
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                dp[i+1][j] += dp[i][j]
+                dp[i][j+1] += dp[i][j]
+        return dp[m-1][n-1]
